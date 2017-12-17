b27467f6c669314164986c3aa0bf18f71ba38950
Pirate021 <rezak04@gmail.com>
2017-12-17 15:55:09 +0330

    Restore the commit "Hill Climbing Class"

---
+++ src/com/company/HillClimbing.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class HillClimbing {
+    private Matrix data;
+    private int[][] status;
+    int h;
+
+
+    public HillClimbing(Matrix problem) {
+        this.data = problem;
+
+    }
+
+    private int Calculate_H(Matrix problem) {
+        int h = 0;
+
+
+        return h;
+    }
+}
